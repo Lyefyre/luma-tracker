0a99ac12bf5541076a3df9a727c6787f16a9927d
Valentin Schär <valentin.schaer@ophion.ch>
2020-09-23 10:23:06 +0200

    New get request attempt

--- src/components/chooseTemtem.vue
+++ src/components/chooseTemtem.vue
@@ -2,11 +2,11 @@
   <div id=select>
       
         <b-dropdown aria-role="list">
-            <button class="button is-primary" slot="trigger" >
+            <button class="button is-primary" slot="trigger" @click="loadAsyncData()">
                 <span>Select Temtem</span>
             </button>
 
-            <b-dropdown-item aria-role="listitem" @click="get_info()">{{ info }}</b-dropdown-item>
+            <b-dropdown-item aria-role="listitem">{{ this.data.results }}</b-dropdown-item>
         </b-dropdown>
   </div>
 </template>
@@ -17,15 +17,29 @@ import axios from "axios";
 export default {
   data() {
     return {
-      info: null
+      data: {
+        results: []
+      },
+      total: 0
     }
   },
   methods: {
-    get_info() {
-      const host = "localhost:8000"
+    loadAsyncData() {
+      let host = `http://localhost:8000/counter/`;
       axios
-        .get(`${host}/counter/`)
-        .then(response => (this.info = response))
+        .get(`${host}`)
+        .then((({ data }) => {
+          this.total = data.count;
+          console.log(this.total);
+          this.data.results = [];
+          data.results.forEach(item => {
+            this.data.results.push(item);
+          });
+        }))
+        .catch(error => {
+          this.data.results = "Error!";
+          throw error;
+        });
     }
   }
 }
